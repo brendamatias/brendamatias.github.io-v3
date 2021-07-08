32ca17d929b3048f593f4605d33b1697a50eff31
brendamatias <brendamatias.sobral@gmail.com>
2021-07-08 15:20:34 -0300

    Updates

---
+++ precache-manifest.8ecc22bbc61fc902d01fe4e20808bcf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7edc76f37369bf9751b8e5e1f18afaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d22b6ffb21b32136a552",
+    "url": "/static/js/2.d192cde2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d192cde2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626439e3599c125a6a6f",
+    "url": "/static/js/main.d60bc089.chunk.js"
+  },
+  {
+    "revision": "f633022a99719ccbad81",
+    "url": "/static/js/runtime-main.46339031.js"
+  },
+  {
+    "revision": "52a99cc622e8ad0645327141704a1748",
+    "url": "/static/media/perfil.52a99cc6.jpg"
+  },
+  {
+    "revision": "eec5d7228cd62e8e3af9504f22bf4d31",
+    "url": "/static/media/roda-acessivel.eec5d722.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5da267bba623a61083834c3db82a818f.js"
+  "/precache-manifest.8ecc22bbc61fc902d01fe4e20808bcf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
